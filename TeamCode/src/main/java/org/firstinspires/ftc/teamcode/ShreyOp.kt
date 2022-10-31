8414a454205a4ea9d52dad4e34561e55f30512e2
12589-PioneerRobotics <12589.programmers@gmail.com>
2022-10-31 16:25:15 -0400

    Added Roadrunner Drive Files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShreyOp.kt
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive
+
+@TeleOp(name = "ShreyOp")
+class ShreyOp : OpMode() {
+    var drive: SampleMecanumDrive? = null
+    override fun init() {
+        telemetry.addLine("Initialized!")
+        telemetry.update()
+        drive = SampleMecanumDrive(hardwareMap)
+    }
+
+    override fun loop() {
+        drive?.setWeightedDrivePower(Pose2d(
+            -gamepad1.left_stick_y.toDouble(),
+            -gamepad1.left_stick_x.toDouble(),
+            -gamepad1.right_stick_x.toDouble()
+        ))
+    }
+}
